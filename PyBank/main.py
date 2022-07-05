e7961f3640077b83677f2fd5261897b2f9db36d0
divyavenkates <divvenkatesh9113@gmail.com>
2022-07-05 19:43:42 -0400

    changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,43 +1,63 @@
+# import os and csv
 import os
-filepath=os.path.join("Resources","budget_data.csv")
 import csv
+# create path for budget_data.csv file 
+filepath=os.path.join("Resources","budget_data.csv")
+# open and read budget_data.csv file 
 with open(filepath,'r') as file: 
     csvreader=csv.reader(file, delimiter=",")
+    #set the first line in budget_data.csv file as header 
     header=next(csvreader)
 
+    #set months and totalprice equal to 0
     months=0
     totalprice=0
-    profit_losses=[]
+    #create empty list for each column in list 
+    profits=[]
     date=[]
+    #create empty list for profit changes between each row 
     changes=[]
+
+    #for each row in budget_data.csv file
     for row in csvreader:
+        #add one to value of months 
         months+=1
+         #add the integer value from Profit/Losses column to value of totalprice
         totalprice += int(row[1])
+        #append the string value from first column to date list 
         date.append(row[0]) 
-        profit_losses.append(int(row[1])) 
+        #append the integer value from Profit/Losses column to date list 
+        profits.append(int(row[1])) 
 
-    for i in range (len(profit_losses)-1):
-        changes.append(profit_losses[i+1] - profit_losses[i])
+    #for every value in profits list
+    for i in range (len(profits)-1):
+        #append the subtraction of that value from the next value to changes list
+        changes.append(profits[i+1] - profits[i])
 
+    #to calculate the average change add all the values from changes list and divide by the number of numbers 
     average = sum(changes)/len(changes) 
  
+    #get index of the maximum and miunimum value of changes list 
     maxindex = changes.index(max(changes))
     minindex = changes.index(min(changes))
 
+    #print these statements
     print("Financial Analysis")
     print("---------------------------- ")  
     print(f"Total Months: {months}")
     print(f"Total: ${totalprice}")
     print(f"Average Change: $ {round(average,2)}")
-    print(f"Greatest Increase in Profits: {date[maxindex+1]} (${max(changes)})")
-    print(f"Greatest Decrease in Profits: {date[minindex+1]} (${min(changes)})")
+    print(f"Greatest Increase in Profits: {date[maxindex+1]} (${changes[maxindex]})")
+    print(f"Greatest Decrease in Profits: {date[minindex+1]} (${changes[minindex]})")
 
+#create a path for a file that will print the analysis for the data 
 outputpath=os.path.join("Analysis", "pybank.txt")
+# open and write the following statements 
 with open(outputpath,'w') as file:
     file.write("Financial Analysis\n")
     file.write("---------------------------- \n")  
     file.write(f"Total Months: {months}\n")
     file.write(f"Total: ${totalprice} \n") 
     file.write(f"Average Change: $ {round(average,2)}\n")
-    file.write(f"Greatest Increase in Profits: {date[maxindex+1]} (${max(changes)})\n")
-    file.write(f"Greatest Decrease in Profits: {date[minindex+1]} (${min(changes)})\n")
+    file.write(f"Greatest Increase in Profits: {date[maxindex+1]} (${changes[maxindex]})\n")
+    file.write(f"Greatest Decrease in Profits: {date[minindex+1]} (${changes[minindex]})\n")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,39 +1,57 @@
+# import os and csv
 import os
-filepath=os.path.join("Resources","election_data.csv")
 import csv
+# create path for election_data.csv file 
+filepath=os.path.join("Resources","election_data.csv")
+
+# open and read election_data.csv file 
 with open(filepath,'r') as file: 
     csvreader=csv.reader(file, delimiter=",")
+    #set the first line in election_data.csv file as header 
     header=next(csvreader)
 
+     #set total votes and each candidates votes equal to 0
     votes=0
     charlesvotes=0
     dianavotes=0
     raymonvotes=0
+     #create empty list for each candidate
     candidates=[]
+
+    #for each row in election_data.csv file
     for row in csvreader:
+        #add one to value of votes
         votes+=1
+        #if a name from candidate column from file is not in candidates list, add it 
         if row[2] not in candidates:
             candidates.append(row[2])   
-                 
+
+        #if the name from candidate column from file is the first name in candidates list, add one to value of charlesvotes
         if row[2] == candidates[0]:
             charlesvotes+=1
+        #if the name from candidate column from file is the second name in candidates list, add one to value of dianavotes
         elif row[2]== candidates[1]:
             dianavotes+=1
+        #if the name from candidate column from file is the third name in candidates list, add one to value of raymon
         elif row[2]== candidates[2]:
             raymonvotes+=1
  
+    #calculate percent of each candidates votes by dividing their votes by total votes and multiplying by 100
     charlespercent = (charlesvotes/votes)*100
     dianapercent = (dianavotes/votes)*100
     raymonpercent = (raymonvotes/votes)*100
 
+    # if charlesvotes is greater than dianavotes and raymonvotes, winner is charles
     if (charlesvotes>dianavotes) & (charlesvotes>raymonvotes):
         winner="Charles Casper Stockham"
+    # if dianavotes is greater than charlesvotes and raymonvotes, winner is diana
     elif (dianavotes>charlesvotes) & (dianavotes>raymonvotes):
         winner="Diana DeGette"
+    # if raymonvotes is greater than charlesvotes and dianavotes, winner is raymon
     elif (raymonvotes>charlesvotes) & (raymonvotes>dianavotes):
         winner="Raymon Anthony Doane"
 
-
+     #print these statements
     print("Election Results")
     print("---------------------------- ")  
     print(f"Total Votes: {votes}")
@@ -45,7 +63,9 @@ with open(filepath,'r') as file:
     print(f"Winner: {winner}") 
     print("---------------------------- ") 
 
+#create a path for a file that will print the analysis for the data 
 outputpath=os.path.join("Analysis", "pypoll.txt")
+# open and write the following statements 
 with open(outputpath,'w') as file:
     file.write("Election Results\n")
     file.write("---------------------------- \n")  
